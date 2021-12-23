10013c89df6edcff135cbd66bda3a28633421911
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-12-23 19:15:14 +0200

    Create db.js

---
+++ src/db/db.js
@@ -0,0 +1,37 @@
+exports.fullEquipmentList = [
+  {
+    id: 1,
+    item: 'Stethoscope',
+    required: 2,
+  },
+  {
+    id: 2,
+    item: 'Gloves',
+    required: 100,
+  },
+  {
+    id: 3,
+    item: 'Boots',
+    required: 1,
+  },
+  {
+    id: 4,
+    item: 'Multi-tool',
+    required: 1,
+  },
+  {
+    id: 5,
+    item: 'Cervical-collar',
+    required: 4,
+  },
+  {
+    id: 6,
+    item: 'Medication-bag',
+    required: 1,
+  },
+  {
+    id: 7,
+    item: 'Ventilators',
+    required: 2,
+  },
+];
