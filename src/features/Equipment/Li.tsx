0ecd609b0d6d2f6fdcdd2dfa74bdab53e53da43f
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-12-23 22:17:52 +0200

    fixed formlist bug

--- src/features/Equipment/Li.tsx
+++ src/features/Equipment/Li.tsx
@@ -4,7 +4,7 @@ export default function Li({data}:{data:{id:number,item:string,required:number}}
     const [current, setCurrent] = useState(0)
     const {id,item,required} = data;
     return (
-        <li className="list-item">
+        <li className="list-item" >
     <h2 className="platform">#{id} - {item}</h2>
     
     <dl className="ad">
@@ -15,7 +15,7 @@ export default function Li({data}:{data:{id:number,item:string,required:number}}
       <dd className="value"><input value={current} onChange={({target})=>{setCurrent(Number(target.value))}} type="number"></input></dd>
       
       <dt className="name">Missing / Extra :</dt>
-      <dd className="value">{current}</dd>
+      <dd className="value">{current-required}</dd>
     </dl>
   </li>
     )
