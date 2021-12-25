1f0363cdab28975c446243a9c255a99a7f91b469
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-12-25 20:48:58 +0200

    prevent send without employee data

--- src/SendBtn.tsx
+++ src/SendBtn.tsx
@@ -6,6 +6,10 @@ export default function SendBtn() {
     const employee = useSelector((state: RootState) => state.employee);
     const equipmentList = useSelector((state: RootState) => state.equipment);
     const handleSend = ()=>{
+        if( Object.keys(employee).length === 0){
+            console.log('missing employee data');
+            return;
+        }
         console.log(employee,equipmentList);
     }
     return (
